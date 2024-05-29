af1bf605220744de7e98bb755a809d19c70cd73a
Bryan11L <danny.gamer11@hotmail.com>
2024-05-29 00:58:54 -0500

    Entrega todo

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const percentageInput = document.getElementById('percentage');
-  const numberInput = document.getElementById('number');
-  const calculateButton = document.getElementById('calculate');
-  const resultElement = document.getElementById('result');
+document.addEventListener('DOMContentLoaded', () => {
+  const getElement = id => document.getElementById(id);
+  const percentageInput = getElement('percentage');
+  const numberInput = getElement('number');
+  const calculateButton = getElement('calculate');
+  const resultElement = getElement('result');
 
   calculateButton.addEventListener('click', () => {
     const percentage = parseFloat(percentageInput.value);
     const number = parseFloat(numberInput.value);
+    const result = (percentage / 100) * number;
 
-    if (isNaN(percentage) || isNaN(number)) {
-      resultElement.textContent = 'Por favor, ingresa números válidos';
-    } else {
-      const result = (percentage / 100) * number;
-      resultElement.textContent = `Resultado: ${result.toFixed(2)}`;
-    }
+    resultElement.textContent = isNaN(percentage) || isNaN(number)
+      ? 'Por favor, ingrese números válidos'
+      : `Resultado: ${Number.isInteger(result) ? result : result.toFixed(2)}`;
   });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = { content:
-  ["./public/**/*.{html,js}"],
-  theme: { 
-    extend: {},
-   }, 
-   plugins: [
-    require('@tailwindcss/forms'),], 
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui',],
+        'serif': ['ui-serif', 'Georgia',],
+        'mono': ['ui-monospace', 'SFMono-Regular',],
+    
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
 };
-
